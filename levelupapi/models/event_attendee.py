d8c954f1bfd310908e4303b0178092c088b483f9
Nick Carver <nicholascarver2@outlook.com>
2021-05-03 15:24:27 -0500

    changed from cascase delete on the join table

--- levelupapi/models/event_attendee.py
+++ levelupapi/models/event_attendee.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class EventAttendee(models.Model):
 
-    gamer = models.ForeignKey("Gamer", on_delete=models.CASCADE)
-    event = models.ForeignKey("Event", on_delete=models.CASCADE)
+    gamer = models.ForeignKey("Gamer", on_delete=models.SET_NULL, null=True)
+    event = models.ForeignKey("Event", on_delete=models.SET_NULL, null=True)
