8f64defab50fd39204c30a1c1406d160f3db462d
Nick Carver <nicholascarver2@outlook.com>
2021-05-03 13:40:32 -0500

    got the classes set up to create tables

--- levelupapi/models/event.py
+++ levelupapi/models/event.py
@@ -2,5 +2,7 @@ from django.db import models
 
 class Event(models.Model):
 
-    game_id = models.ForeignKey(Game, on_delete=models.CASCADE)
-    event_time = models.models.DateTimeField(_("what"), auto_now=False, auto_now_add=False)
+    game = models.ForeignKey("Game", on_delete=models.CASCADE)
+    event_time = models.DateTimeField(auto_now=False, auto_now_add=False)
+    location = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
--- levelupapi/models/game.py
+++ levelupapi/models/game.py
@@ -4,4 +4,7 @@ from django.db import models
 class Game(models.Model):
 
     name = models.CharField(max_length=50)
-    game_type_id = models.ForeignKey(GameType, on_delete=models.CASCADE)
+    game_type = models.ForeignKey(GameType, on_delete=models.CASCADE)
+    max_players = models.IntegerField()
+    min_players = models.ForeignKey(GameType, on_delete=models.CASCADE)
+    difficulty = models.CharField(max_length=50)
