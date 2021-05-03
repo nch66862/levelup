9063826b233de7ee689f821fccaf54c68dff128e
Nick Carver <nicholascarver2@outlook.com>
2021-05-03 15:45:09 -0500

    another migration with the changes to the table

--- levelupapi/models/event_attendee.py
+++ levelupapi/models/event_attendee.py
@@ -3,4 +3,4 @@ from django.db import models
 class EventAttendee(models.Model):
 
     gamer = models.ForeignKey("Gamer", on_delete=models.SET_NULL, null=True)
-    event = models.ForeignKey("Event", on_delete=models.SET_NULL, null=True)
+    event = models.ForeignKey("Event", on_delete=models.DO_NOTHING, null=True)
