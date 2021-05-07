4b5de59a7c3331038f9216a0d1f0c231a952c7e3
Nick Carver <nicholascarver2@outlook.com>
2021-05-07 11:47:27 -0500

    got the profile to return things all from the gamer model and using a depth to show all of the events that they are attending.

--- levelup/urls.py
+++ levelup/urls.py
@@ -17,12 +17,13 @@ from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
 from rest_framework import routers
-from levelupapi.views import GameTypes, Games, Events, register_user, login_user
+from levelupapi.views import GameTypes, Games, Events, register_user, login_user, Profile
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'gametypes', GameTypes, 'gametype')
 router.register(r'games', Games, 'game')
 router.register(r'events', Events, 'event')
+router.register(r'profile', Profile, 'profile')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
