84358f2291adc20993caf681daf0d05495a4c12b
Nick Carver <nicholascarver2@outlook.com>
2021-05-26 14:41:31 -0500

    refactored to use a view stored in the database instead of an individual query

--- levelupreports/views/users/eventsbyuser.py
+++ levelupreports/views/users/eventsbyuser.py
@@ -16,24 +16,16 @@ def userevent_list(request):
             # Query for all games, with related user info.
             db_cursor.execute("""
                 SELECT
-                    e.id as event_id,
-                    e.event_time,
-                    e.location,
-                    e.name as event_name,
-                    g.title as game_name,
-                    gr.id gamer_id,
-                    u.id as user_id,
-                    u.first_name || ' ' || u.last_name AS full_name
+                    event_id,
+                    event_time,
+                    location,
+                    event_name,
+                    game_name,
+                    gamer_id,
+                    user_id,
+                    full_name
                 FROM
-                    levelupapi_event e
-                JOIN
-                    levelupapi_eventattendee ea ON ea.event_id = e.id
-                JOIN
-                    levelupapi_gamer gr ON ea.gamer_id = gr.id
-                JOIN
-                    auth_user u ON gr.user_id = u.id
-                JOIN
-                    levelupapi_game g ON g.id = e.game_id
+                    EVENTS_BY_USER
             """)
 
             dataset = db_cursor.fetchall()
--- test.sql
+++ test.sql
@@ -1,7 +1,29 @@
+CREATE VIEW GAMES_BY_USER AS
 SELECT
-    e.id,
-    e.name,
+    g.id,
+    g.title,
+    g.maker,
+    g.gametype_id,
+    g.number_of_players,
+    g.skill_level,
     u.id user_id,
+    u.first_name || ' ' || u.last_name AS full_name
+FROM
+    levelupapi_game g
+JOIN
+    levelupapi_gamer gr ON g.gamer_id = gr.id
+JOIN
+    auth_user u ON gr.user_id = u.id
+;
+CREATE VIEW EVENTS_BY_USER AS
+SELECT
+    e.id as event_id,
+    e.event_time,
+    e.location,
+    e.name as event_name,
+    g.title as game_name,
+    gr.id gamer_id,
+    u.id as user_id,
     u.first_name || ' ' || u.last_name AS full_name
 FROM
     levelupapi_event e
@@ -11,3 +33,5 @@ JOIN
     levelupapi_gamer gr ON ea.gamer_id = gr.id
 JOIN
     auth_user u ON gr.user_id = u.id
+JOIN
+    levelupapi_game g ON g.id = e.game_id;
